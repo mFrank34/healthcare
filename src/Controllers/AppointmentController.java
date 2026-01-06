3e50bfb1111124eb4a532366619db166d2328ac1
Michael Frank <mffranks34@gmail.com>
2026-01-06 12:49:35 +0000

    refactoring form into FormPanel

--- src/Controllers/AppointmentController.java
+++ src/Controllers/AppointmentController.java
@@ -1,5 +1,19 @@
 package Controllers;
 
+import Models.Appointment;
+import Validation.AppointmentValidator;
+import Views.AppointmentView;
+
 public class AppointmentController {
+    private final Appointment model;
+    private final AppointmentView view;
+    private final AppointmentValidator validator;
+
+    public AppointmentController(Appointment model, AppointmentView view, AppointmentValidator validator) {
+        this.model = model;
+        this.view = view;
+        this.validator = validator;
+    }
+
 
 }
--- src/Views/AppointmentView.java
+++ src/Views/AppointmentView.java
@@ -1,4 +1,27 @@
 package Views;
 
-public class AppointmentView {
+import Controllers.AppointmentController;
+import FactoryUI.Form;
+import javax.swing.*;
+
+public class AppointmentView extends JFrame {
+    private AppointmentController controller;
+    private Form appointmentForm;
+
+
+    public AppointmentView() {
+        setTitle("Appointment Form");
+        setSize(900, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public void setController(AppointmentController controller) {
+        this.controller = controller;
+        initComponents();
+    }
+
+    private void initComponents() {
+        appointmentForm =
+    }
 }
